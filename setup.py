8f2b3ebc4703b9401b633155ff9443fecd77e8e6
Helmut Konrad Schewe <helmutus@outlook.com>
2023-01-03 11:18:50 +0000

    chore(setup): use quick install

--- setup.py
+++ setup.py
@@ -8,43 +8,12 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-UTF8 = 'utf8'
-
-with open(os.path.join(ROOT, 'README.md'), mode='rt', encoding=UTF8) as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'elements/__init__.py'), encoding=UTF8) as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding=UTF8) as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = [
+    'elements',
+    'elements.headline',
+]
 
 if __name__ == "__main__":
-    # allow setup.py to run from another directory
-    os.chdir(ROOT)
-    setuptools.setup(
-        author='Helmut Konrad Schewe',
-        author_email='info@checkitweg.de',
-        description='check mich',
-        install_requires=REQUIRES,
-        long_description=README,
-        name='elements',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/elements',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        packages=[
-            'elements',
-            'elements.headline',
-        ],
-    )
+    utila.install(__file__)
